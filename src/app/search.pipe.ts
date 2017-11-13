381de5c9b36b21e33a528b54a6a456746a69f450
Himanshu Singh <himanshu.singh@nagarro.com>
2017-11-14 01:40:33 +0530

    implemented search box(by blog title)

---
+++ src/app/search.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+
+  transform(blogs: Object[], searchText?: string): any {
+    if (!blogs) {
+      return [];
+    }
+    if (!searchText) {
+      return blogs;
+    }
+    searchText = searchText.toLowerCase();
+    return blogs.filter( blog => {
+      return blog['title'].toLowerCase().includes(searchText);
+    });
+  }
+
+}
