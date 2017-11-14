63e3c07a5ad845e7b6c358db628937e3552e4123
Himanshu Singh <singh.himanshu2408@gmail.com>
2017-11-14 18:50:38 +0530

    minor changes

--- src/app/base/home/favourite-blogs/favourite-blogs.component.ts
+++ src/app/base/home/favourite-blogs/favourite-blogs.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-favourite-blogs',
@@ -8,9 +8,13 @@ import {Component, Input, OnInit} from '@angular/core';
 export class FavouriteBlogsComponent implements OnInit {
 
   @Input() blogs;
+  @Output() notify: EventEmitter<Object> = new EventEmitter<Object>();
   constructor() { }
 
   ngOnInit() {
+  }
+  selectBlog(blog) {
+    this.notify.emit(blog);
   }
 
 }
--- src/assets/script.js
+++ src/assets/script.js
@@ -1,4 +1,7 @@
 $( document ).ready(function(){
   $(".dropdown-button").dropdown();
   $(".button-collapse").sideNav();
+  $(document).ready(function() {
+    $('select').material_select();
+  });
 });
